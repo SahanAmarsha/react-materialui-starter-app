ce4c7ffca68209741e3f488e41b7612e64836093
Sahan Amarsha <sahanamarsha@gmail.com>
2022-03-02 20:00:21 +0530

    Add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {CssBaseline, Typography} from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+        style={{
+            flex: '1',
+            display: 'flex',
+            minHeight: '100vh',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}
+    >
+        <CssBaseline />
+        <Typography variant="h2">
+            Starter App
+        </Typography>
     </div>
   );
 }
